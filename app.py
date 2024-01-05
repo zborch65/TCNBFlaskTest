ccd145ed925876acb4af8bba1386484d83677193
zborch65 <130394049+zborch65@users.noreply.github.com>
2024-01-05 10:50:55 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'tcnb65421@@'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        busname = request.form['busname']
+        busacctnum = request.form['busacctnum']
+        print(busname, busacctnum)  # do something here?
+
+        f = open('static\\comments.csv', 'a')
+        f.write(",".join([busname, busacctnum]))
+        f.close()
+
+        return redirect(url_for('test'))
+    return render_template('index.html')
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def session():
+    return render_template('index.html')
+
+
+
+
+
+
+
+
+

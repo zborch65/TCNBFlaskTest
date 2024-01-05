909624b4ec542a82f6537b5b2ceaae4471e7c6ee
zborch65 <130394049+zborch65@users.noreply.github.com>
2024-01-05 11:13:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app.config['SECRET_KEY'] = 'tcnb65421@@'
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
-        busname = request.form['busname']
-        busacctnum = request.form['busacctnum']
-        print(busname, busacctnum)  # do something here?
+        busname = request.form.get('busname')
+        busacctnum = request.form.get('busacctnum')
+        print(busname, '\n', busacctnum)
 
         
     return render_template('index.html')
@@ -21,6 +21,10 @@ def main():
 @app.route('/test', methods=['GET', 'POST'])
 def session():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
